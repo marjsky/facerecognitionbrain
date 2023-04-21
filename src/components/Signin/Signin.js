bbe89483b8d934d1470f5838bb9dc5e67210c8d9
marjsky <67877013+marjsky@users.noreply.github.com>
2023-04-20 22:13:53 -0400

    correct variables in codebase of signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -4,36 +4,37 @@ class Signin extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      signinEmail: '',
-      signinPassword: ''
-    }
+      signInEmail: "",
+      signInPassword: "",
+    };
   }
 
   onEmailChange = (event) => {
-    this.setState({signinEmail: event.target.value})
+    this.setState({signInEmail: event.target.value})
   }
 
   onPasswordChange = (event) => {
-    this.setState({signinPassword: event.target.value})
+    this.setState({signInPassword: event.target.value})
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
+    fetch("http://localhost:3000/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        email: this.state.signinEmail,
-        password: this.state.signinPassword
-      })
+        email: this.state.signInEmail,
+        password: this.state.signInPassword,
+      }),
     })
-      .then(response => response.json())
-      .then(data => {
-        if ( data === 'success') {
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id) {
+          this.props.loadUser(user);
           this.props.onRouteChange("home");
         }
-      })
+      });
   }
-  
+
   render () {
     const { onRouteChange } = this.props;
     return (
